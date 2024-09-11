821ac6840cccdc750894ad786f091027f8fd8d4d
c20mln <c20mln@cs.umu.se>
2024-09-11 18:50:40 +0200

    dont know if this even works but should be somewhat like the textbook algorithms

--- src/Parser.java
+++ src/Parser.java
@@ -30,18 +30,19 @@ public class Parser {
         int n = charArray.length;
         int S = theGrammar.getNonterminalInt(theGrammar.getFirstNonterminal());
         if (charArray != null && n != 0) {
+            counter = 0;
             parseNaive(S, 0, n);
         }
     }
 
-    public boolean parseNaive(int A, int i, int j) {
-        counter = 0;
+    public boolean parseNaive(int S, int i, int j) {
         if (i == j - 1) {
+            counter++;
             char terminal = charArray[i];
             List<String> terminalRules = theGrammar.getTerminalRules(terminal);
-            return terminalRules.contains(theGrammar.getNonterminalString(A));
+            return terminalRules.contains(theGrammar.getNonterminalString(S));
         } else {
-            List<int[]> nonterminalRules = theGrammar.getNonterminalRules(A);
+            List<int[]> nonterminalRules = theGrammar.getNonterminalRules(S);
             for (int[] rule : nonterminalRules) {
                 int B = rule[0];
                 int C = rule[1];
@@ -57,10 +58,79 @@ public class Parser {
     }
 
     public boolean parseBU(int S, int i, int j) {
+        int n = charArray.length;
+        Boolean[][][] table = new Boolean[theGrammar.getAllNonterminalRules().length][n][n + 1];
 
+        fillTableBU(n, table);
+
+        for (int span = 2; span <= n; span++) {
+            for (int start = 0; start <= n - span; start++) {
+                int end = start + span;
+                for (int split = start + 1; split < end; split++) {
+                    for (int A = 0; A < theGrammar.getAllNonterminalRules().length; A++) {
+                        List<int[]> nonterminalRules = theGrammar.getNonterminalRules(A);
+                        for (int[] rule : nonterminalRules) {
+                            counter++;
+                            int B = rule[0];
+                            int C = rule[1];
+                            if (table[B][start][split] && table[C][split][end]) {
+                                table[A][start][end] = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return table[S][i][j];
     }
 
-    public boolean parseTD() {
+    private void fillTableBU(int n, Boolean[][][] table) {
+        for (int s = 0; s < n; s++) {
+            char terminal = charArray[s];
+            List<String> terminalRules = theGrammar.getTerminalRules(terminal);
+            for (String nonterminal : terminalRules) {
+                int A = theGrammar.getNonterminalInt(nonterminal);
+                table[A][s][s + 1] = true;
+            }
+        }
+    }
 
+    public boolean parseTD(int S, int i, int j) {
+        int n = charArray.length;
+        Boolean[][][] table = new Boolean[theGrammar.getAllNonterminalRules().length][n][n + 1];
+
+        if (table[S][i][j] != null) {
+            return table[S][i][j];
+        }
+
+        boolean result;
+        if (i == j - 1) {
+            counter++;
+            char terminal = charArray[i];
+            List<String> terminalRules = theGrammar.getTerminalRules(terminal);
+            result = terminalRules.contains(theGrammar.getNonterminalString(S));
+        } else {
+            result = false;
+            List<int[]> nonterminalRules = theGrammar.getNonterminalRules(S);
+            for (int[] rule : nonterminalRules) {
+                int B = rule[0];
+                int C = rule[1];
+                for (int k = i + 1; k < j; k++) {
+                    counter++;
+                    if (parseTD(B, i, k) && parseTD(C, k, j)) {
+                        result = true;
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+        }
+
+        table[S][i][j] = result;
+        return table[S][i][j];
     }
 }
